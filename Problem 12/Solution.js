93d9dd2e21ed7a8eb6f307c5212a948e1123df4c
MadJack1906 <cho.suhoond.74@gmail.com>
2022-07-28 17:15:15 +0800

    Added Solution for Problem 12

---
+++ Problem 12/Solution.js
@@ -0,0 +1,14 @@
+let string = "asdasdasd";
+let stringArray = Array.from(string);
+let letterOccurrences = []; // key value pair of occurrence of a letter e.g. a: 1
+
+let letterSet = new Set(stringArray);
+
+letterSet.forEach(element => {
+    let regex = new RegExp(element, "g");
+
+    letterOccurrences[element] = string.match(regex).length;
+});
+
+console.log(letterOccurrences);
+
