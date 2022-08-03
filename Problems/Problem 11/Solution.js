5074fad2a745f812c8dfa9776f0f936fb221e9f5
MadJack1906 <cho.suhoond.74@gmail.com>
2022-08-04 02:34:27 +0800

    Created index.html and Problems folder

--- Problems/Problem 11/Solution.js
+++ Problems/Problem 11/Solution.js
@@ -7,27 +7,15 @@ function amountToCoins(amount, coins){
     });
     //console.log(coins)
     let amountCoins = [];
-    let currentValue = 0;
 
-    //console.log(coins)
-
-    while(true){
-        if (amount > coins[0]) {
-
+    while (amount != 0) {
+        if (amount >= coins[0]){
+            amount -= coins[0];
             amountCoins.push(coins[0]);
-            currentValue += coins[0];
-
         } else {
-
             coins.splice(0, 1);
-
         }
-
-        console.log(amountCoins);
-        console.log(currentValue);
-
-        
     }
     
-    //return amountCoins;
+    return amountCoins;
 }
